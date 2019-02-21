f4bbdd56429062707729ccc1e438064d1a183a44
Ori Harel <ori.harel@gmail.com>
2019-02-21 21:42:34 +0200

    battery level

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const gpio = require('onoff').Gpio;
+const batteryLevel = require('battery-level');
+
+batteryLevel().then(level => {
+  console.log(`Battery level is ${level*100}%`);
+});
 
 
 const blue = new gpio(10, "out");
