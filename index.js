5a0b5fa54d9a7103a1b729e78de18a567915967e
Ori Harel <ori.harel@gmail.com>
2019-02-22 23:06:17 +0200

    sound code

--- index.js
+++ index.js
@@ -5,43 +5,68 @@ const blue = new gpio(10, "out");
 const red = new gpio(9, "out");
 const green = new gpio(11, "out");
 
-const sleep = (howLong) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, howLong)
-  })
-};
+const sound = new gpio(26, "in", "both");
 
+sound.watch((err, value)=>{
 
-const runLights = async () => {
-  while (true) {
-    // Red
-    red.writeSync(1)
-    await sleep(3000)
-
-    // Red and Blue
-    blue.writeSync(1)
-    await sleep(1000)
-    //
-    // Green
-    red.writeSync(0)
-    blue.writeSync(0)
-    green.writeSync(1)
-    await sleep(5000)
-    //
-    // Blue
-    green.writeSync(0)
-    blue.writeSync(1)
-    await sleep(2000)
-    //
-    // Blue off
-    blue.writeSync(0)
+  if (err) {
+    console.error(`Error reading sound ${err}`);
   }
-};
+  else {
+    console.log(`received sound value ${value}`);
+    if (value === 1) {
+      allLightsOn();
+    }
+    else {
+      allLightsOff();
+    }
+  }
+
+})
+
+// const sleep = (howLong) => {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, howLong)
+//   })
+// };
+
+
+// const runLights = async () => {
+//   while (true) {
+//     // Red
+//     red.writeSync(1)
+//     await sleep(3000)
+//
+//     // Red and Blue
+//     blue.writeSync(1)
+//     await sleep(1000)
+//     //
+//     // Green
+//     red.writeSync(0)
+//     blue.writeSync(0)
+//     green.writeSync(1)
+//     await sleep(5000)
+//     //
+//     // Blue
+//     green.writeSync(0)
+//     blue.writeSync(1)
+//     await sleep(2000)
+//     //
+//     // Blue off
+//     blue.writeSync(0)
+//   }
+// };
 
 const allLightsOff = () => {
   blue.writeSync(0)
   red.writeSync(0)
   green.writeSync(0)
+};
+
+const allLightsOn = () => {
+  blue.writeSync(1)
+  red.writeSync(1)
+  green.writeSync(1)
 };
 
 // Handle Ctrl+C exit cleanly
@@ -51,4 +76,4 @@ process.on('SIGINT', () => {
 });
 
 allLightsOff();
-runLights();
+// runLights();
